0bd26d7963b4a40bb4292bbe9bb3f26fb748656e
Justin McMillion <79409949+jmcmillion89@users.noreply.github.com>
2024-08-18 11:18:57 -0400

    add components. add styling.

---
+++ src/CV.jsx
@@ -0,0 +1,22 @@
+
+function CV () {
+  return(
+    <div className='cv'>
+      
+      <div className='cv-personal-details'>
+        Personal Details
+      </div>
+
+      <div className='cv-education'>
+        Education
+      </div>
+
+      <div className='cv-experience'>
+        Experience
+      </div>
+
+    </div>
+  )
+}
+
+export default CV;
---
+++ src/SideBar.jsx
@@ -0,0 +1,25 @@
+
+function SideBar() {
+  return (
+    <div className='sidebar'>
+
+      <div className='sidebar-options'>
+          Options
+      </div>
+
+      <div className='sidebar-personal-details'>
+        Personal Details
+      </div>
+
+      <div className='sidebar-education'>
+        Education
+      </div>
+
+      <div className='sidebar-experience'>
+        Experience
+      </div>
+    </div>
+  )
+}
+
+export default SideBar
