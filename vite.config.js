d6a8fa5c3e5cf325539bce4fa34f71e809a85625
jmcmillion89 <79409949+jmcmillion89@users.noreply.github.com>
2024-09-07 19:05:05 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["zustand", "zustand/middleware"],
+      external: ["zustand", "zustand/middleware", "uuid"],
     },
   },
 });
